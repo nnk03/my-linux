6d38d43610154091d91aef0c783dbf4dde6d4243
Neeraj Krishna N <neerajnjps03@gmail.com>
2024-08-27 21:32:23 +0530

    update nvim-cmp.lua and added mappings for jumping to next and previous params in autocomplete feature

--- ubuntu/.config/nvim/lua/neeraj/plugins/nvim-cmp.lua
+++ ubuntu/.config/nvim/lua/neeraj/plugins/nvim-cmp.lua
@@ -37,6 +37,26 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        -- jump to next params in autocompletion
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
