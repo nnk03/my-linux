d8e03dfb6e33bfbd9b1bf64a24dc1a73136fb292
Neeraj Krishna N <neerajnjps03@gmail.com>
2024-10-04 21:49:06 +0530

    added black wallpaper when locking the screen for screen pixel health, yet to debug why it is not going to sleep automatically

--- ubuntu/.config/sway/lock_screen.sh
+++ ubuntu/.config/sway/lock_screen.sh
@@ -7,17 +7,17 @@
 # Times the screen off and puts it to background
 swayidle -w \
 	before-sleep 'loginctl lock-session $XDG_SESSION_ID' \
-	lock 'swaylock -i $HOME/.config/sway/wallpaper.png'  \
-    timeout 600 $lock \
-    timeout 570 'swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' \
-    before-sleep $lock \
+	lock 'swaylock -i $HOME/.config/sway/wallpaper.png' \
+	timeout 600 $lock \
+	timeout 570 'swaymsg "output * dpms off"' \
+	resume 'swaymsg "output * dpms on"' \
+	before-sleep $lock \
 	swayidle \
-    timeout 10 'swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' &
+	timeout 10 'swaymsg "output * dpms off"' \
+	resume 'swaymsg "output * dpms on"' &
 
 # Locks the screen immediately
-swaylock -i $HOME/.config/sway/ubuntu_wallpaper.png -c 550000
+swaylock -i $HOME/.config/sway/BlackWallpaper.jpg -c 550000
 
 # Kills last background task so idle timer doesn't keep running
 kill %%
